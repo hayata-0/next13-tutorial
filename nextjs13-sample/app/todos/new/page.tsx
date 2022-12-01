6bdad1d257eaebb135a40bb43b2a110edbdb2df8
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-12-01 11:22:51 +0900

    first commit

---
+++ nextjs13-sample/app/todos/new/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { useState } from "react";
+
+export default function Page() {
+  const [value, setValue] = useState("");
+
+  return (
+    <>
+      <h1>New Todo</h1>
+      <form
+        onSubmit={() => {
+          alert("Submitted!");
+        }}
+      >
+        <input
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+        <input type="submit" />
+      </form>
+    </>
+  );
+}
